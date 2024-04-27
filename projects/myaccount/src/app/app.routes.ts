be99003c2636c18157f8bf4b926ddc9e813d935c
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-04-28 06:11:13 +0700

    chore: remove pages

--- projects/myaccount/src/app/app.routes.ts
+++ projects/myaccount/src/app/app.routes.ts
@@ -1,16 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () =>
-      import('./feat/home/home.module').then((m) => m.HomeModule),
-  },
-  {
-    path: 'device-activity',
-    loadChildren: () =>
-      import('./feat/device-activity/device-activity.module').then(
-        (m) => m.DeviceActivityModule,
-      ),
-  },
-];
+export const routes: Routes = [];
