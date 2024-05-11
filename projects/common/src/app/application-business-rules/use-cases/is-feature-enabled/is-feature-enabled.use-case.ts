a104b8d9f1327d14fc09416978183e28ebdfdb59
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-05-11 22:20:46 +0700

    chore: typo

--- projects/common/src/app/application-business-rules/use-cases/is-feature-enabled/is-feature-enabled.use-case.ts
+++ projects/common/src/app/application-business-rules/use-cases/is-feature-enabled/is-feature-enabled.use-case.ts
@@ -1,13 +1,10 @@
-import { Inject, Injectable } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { IsFeatureEnabledDataSource } from '@common/application-business-rules/data-sources/is-feature-enabled/is-feature-enabled.data-source';
 import { IsFeatureEnabledResponse } from '@common/entities/is-feature-enabled/is-feature-enabled.entity';
 
 @Injectable()
 export class IsFeatureEnabledUseCase {
-  constructor(
-    @Inject(IsFeatureEnabledDataSource)
-    private dataSource: IsFeatureEnabledDataSource,
-  ) {}
+  constructor(private dataSource: IsFeatureEnabledDataSource) {}
 
   execute(feature: string): Promise<IsFeatureEnabledResponse> {
     return this.dataSource.execute(feature);
