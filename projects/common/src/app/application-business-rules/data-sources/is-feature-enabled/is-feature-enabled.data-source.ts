b6dabcd8765a1a1de0bd11729add9f37c7f251cb
Jason Rich Darmawan <63768126+jasonrichdarmawan@users.noreply.github.com>
2024-05-12 00:29:02 +0700

    chore: conform to clean architecture (#42)
    * chore: conform
    
    * chore: conform

--- projects/common/src/app/application-business-rules/data-sources/is-feature-enabled/is-feature-enabled.data-source.ts
+++ projects/common/src/app/application-business-rules/data-sources/is-feature-enabled/is-feature-enabled.data-source.ts
@@ -1,5 +1,18 @@
-import { IsFeatureEnabledResponse } from '@common/entities/is-feature-enabled/is-feature-enabled.entity';
+import { IsFeatureEnabledErrorCode } from '@common/entities/is-feature-enabled/is-feature-enabled.entity';
 
 export abstract class IsFeatureEnabledDataSource {
-  abstract execute(feature: string): Promise<IsFeatureEnabledResponse>;
+  abstract execute(feature: string): Promise<IsFeatureEnabledModelResponse>;
+}
+
+export type IsFeatureEnabledModelResponse =
+  | {
+      ok: true;
+      data: IsFeatureEnabledModelData;
+    }
+  | { ok: false; errorCode: IsFeatureEnabledErrorCode };
+
+export interface IsFeatureEnabledModelData {
+  development?: boolean;
+  staging?: boolean;
+  production?: boolean;
 }
